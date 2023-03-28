a0cf9dfa3ca06096d8dbc8a15b5bedaa4854b336
Kyle Lord <lordkyle@icloud.com>
2023-03-28 22:51:05 +0100

    first commit

---
+++ Gradebook.py
@@ -0,0 +1,43 @@
+# Last semesters gradebook.
+
+last_semester_gradebook = [["politics", 80], ["latin", 96], ["dance", 97], ["architecture", 65]]
+
+# Mid-Year Gradebook.
+
+# 1) A list of this semesters subjects:
+
+subjects = ["physics", "calculus", "poetry", "history"]
+
+# 2) A list of this semesters grades:
+
+grades = [98, 97, 85, 88]
+
+# 3) A list combining subjects and grades = This semesters gradebook:
+
+gradebook = [["physics", 98], ["calculus", 97], ["poetry", 85], ["history", 88]]
+
+# 4) Your grade for your computer science class just came in! You got a perfect score, 100!:
+
+gradebook.append(["computer science", 100])
+
+# 5) Your grade for "visual arts" just came in! You got a 93!:
+
+gradebook.append(["visual arts", 93])
+
+# 6) Our instructor just told us they made a mistake grading and are rewarding an extra 5 points, for our visual arts class:
+
+gradebook[-1][-1] += 5
+
+# 7) You decided to switch from a numerical grade value to a Pass/Fail option for your poetry class:
+
+gradebook[2].remove(85)
+
+# 8) Use the .append() method, to then add a new "Pass" value to the sublist where your poetry class is located:
+
+gradebook[2].append("Pass")
+
+# 9) You also have your grades from last semester, stored in last_semester_gradebook. Combine the two gradebooks, to create a mid-year gradebook:
+
+full_gradebook = last_semester_gradebook + gradebook 
+
+print(full_gradebook)
---
+++ README.md
@@ -0,0 +1,5 @@
+# Gradebook Project
+
+## Part of 'Learn Python 3 - Codecademy Projects'
+
+I have written a Python program to organize a students subjects and grades for half of an academic year - creating a mid-term gradebook.
